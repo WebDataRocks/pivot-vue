7dc81aa3c35e89047e83689f6c01ca3be72af42e
Flexmonster <github@flexmonster.com>
2019-12-03 11:17:58 +0200

    Added WebDataRocks.vue wrapper

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <WebDataRocks report="https://cdn.webdatarocks.com/reports/report.json" toolbar></WebDataRocks>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WebDataRocks from './components/WebDataRocks.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    WebDataRocks
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/WebDataRocks.vue
@@ -0,0 +1,67 @@
+<template>
+    <div>The component will appear here</div>
+</template>
+
+<script>
+import WebDataRocks from "webdatarocks";
+export default {
+  props: {
+    afterchartdraw: Function,
+    aftergriddraw: Function,
+    beforegriddraw: Function,
+    beforetoolbarcreated: Function,
+    cellclick: Function,
+    celldoubleclick: Function,
+    componentFolder: String,
+    customizeCell: Function,
+    customizeContextMenu: Function,
+    datachanged: Function,
+    dataerror: Function,
+    datafilecancelled: Function,
+    dataloaded: Function,
+    fieldslistclose: Function,
+    fieldslistopen: Function,
+    filterclose: Function,
+    filteropen: Function,
+    fullscreen: Function,
+    global: Object,
+    height: [String, Number],
+    loadingdata: Function,
+    loadinglocalization: Function,
+    loadingolapstructure: Function,
+    loadingreportfile: Function,
+    localizationerror: Function,
+    localizationloaded: Function,
+    olapstructureerror: Function,
+    olapstructureloaded: Function,
+    openingreportfile: Function,
+    querycomplete: Function,
+    queryerror: Function,
+    ready: Function,
+    report: [String, Object],
+    reportchange: Function,
+    reportcomplete: Function,
+    reportfilecancelled: Function,
+    reportfileerror: Function,
+    reportfileloaded: Function,
+    runningquery: Function,
+    toolbar: Boolean,
+    update: Function,
+    width: [Number, String]
+  },
+  mounted: function() {
+    this.webdatarocks = new WebDataRocks({
+      ...this.$props,
+      container: this.$el
+    });
+  },
+  beforeUpdate() {
+    return false;
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+@import '~webdatarocks/webdatarocks.min.css';
+</style>
